cf1d6a8389dc7c73f4395625d88f0e3f255129d2
Ubuntu <ubuntu@ip-172-31-28-47.ap-southeast-2.compute.internal>
2018-07-11 11:08:57 +0000

    prepare Demo

--- model.py
+++ model.py
@@ -24,25 +24,17 @@ class DecoderRNN(nn.Module):
         self.embed = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, 1, batch_first=True)
         self.softmax = nn.Softmax()
-
         self.linear = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, features, captions):
-        # print("Captions      ======", captions.shape)
         embeddings = self.embed(captions[:, :-1])
-        # print("Embeddings    ======", embeddings.shape)
-
         features = features.unsqueeze(1)
-        #print("Features      ======", features.shape)
+        
         embeddings = torch.cat((features, embeddings), 1)
-        #print("Concat        ======", embeddings.shape)
-
+        
         lstm_out, self.hidden = self.lstm(embeddings)
-        #print("LSTM out      ======", lstm_out.shape)
-
+        
         output = self.linear(lstm_out)
-
-        # output = output[:, 1:, :].contiguous() # do not return CNN output (<start> word)
 
         return output
 
@@ -58,19 +50,11 @@ class DecoderRNN(nn.Module):
         embeddings = inputs
 
         while count < max_len and prediction != 1:
-            #print("embedding shape ============", embeddings.shape)
-            #print(embeddings)
             lstm_out, states = self.lstm(embeddings, states)
             output = self.linear(lstm_out)
 
-            #print("output =====================", output.shape)
-            # output = output[:, -1:, :]
-
             max_p, pred = output.max(2)
 
-            #print("prediction =================", max_p.data[0].tolist())
-            #print("index ======================", pred.data[0].tolist())
-            #print("\n")
             prediction = pred.item()
             preds.append(prediction)
             embeddings = self.embed(pred)
